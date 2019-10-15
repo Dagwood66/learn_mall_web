721ad38a2e2ee879bd81f55cf8805afbec7ba4b5
李子豪 <15836784857@163.com>
2019-10-15 15:26:11 +0800

    用户登出

--- src/pages/My.vue
+++ src/pages/My.vue
@@ -1,18 +1,28 @@
 <template>
     <div class="my">
-        <h1>我的</h1>
-        <mt-button type="default" size="large" @click="goLogin">登陆</mt-button>
+        <template v-if="isLogin">
+            <div style="text-align: center;">
+                <el-avatar :size="60" :src="dataInfo.avatar_url"></el-avatar>
+                <p>{{dataInfo.phone}}</p>
+            </div>
+            <mt-button type="default" size="large" @click="loginOut">退出账户</mt-button>
+        </template>
+        <template v-else>
+            <mt-button type="default" size="large" @click="goLogin">登陆</mt-button>
+        </template>
         <mt-button type="default" size="large" @click="goOrders">我的订单</mt-button>
     </div>
 </template>
 
 <script>
     import {mapState} from "vuex"
+    import axios from "axios"
 
     export default {
         data() {
             return {
-                dataList: []
+                dataInfo: {},
+                isLogin: false,
             }
         },
         computed: {
@@ -21,14 +31,35 @@
             })
         },
         mounted() {
-
+            axios.get("/api/login").then(res => {
+                this.isLogin = res.data.status === "1";
+                if (this.isLogin) {
+                    this.getDataInfo();
+                }
+            }, res => {
+                this.isLogin = false;
+            })
         },
         methods: {
+            getDataInfo() {
+                axios.get("/api/user").then(res => {
+                    this.dataInfo = res.data.data;
+                })
+            },
             goLogin() {
                 this.$router.push("/login")
             },
             goOrders() {
                 this.$router.push("/orders")
+            },
+            loginOut() {
+                axios.get("/api/login", {
+                    params: {
+                        type: "loginOut",
+                    }
+                }).then(res => {
+                    console.log(res);
+                })
             }
         }
     }
